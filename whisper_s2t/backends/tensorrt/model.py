586c594efee66bec88c02dffa11b69e2b1b535ea
bofenghuang <bofenghuang7@gmail.com>
2025-04-16 09:23:06 +0000

    fix trt

--- whisper_s2t/backends/tensorrt/model.py
+++ whisper_s2t/backends/tensorrt/model.py
@@ -230,7 +230,7 @@ class WhisperModelTRT(WhisperModel):
 
         return word_timings
     
-    def generate_segment_batched(self, features, prompts, seq_lens, seg_metadata):
+    def generate_segment_batched(self, features, align_features, prompts, seq_lens, seg_metadata):
 
         result = self.model.generate(features,
                                      prompts,
@@ -245,7 +245,8 @@ class WhisperModelTRT(WhisperModel):
         if self.asr_options['word_timestamps']:
             text_tokens = [[_t for _t in x[0] if _t < self.tokenizer.eot]+[self.tokenizer.eot] for x in result]
             sot_seqs = [tuple(_[-4:]) for _ in prompts]
-            word_timings = self.align_words(features, texts, text_tokens, sot_seqs, seq_lens, seg_metadata)
+            # word_timings = self.align_words(features, texts, text_tokens, sot_seqs, seq_lens, seg_metadata)
+            word_timings = self.align_words(align_features, texts, text_tokens, sot_seqs, seq_lens, seg_metadata)
 
             for _response, _word_timings in zip(response, word_timings):
                 _response['word_timestamps'] = _word_timings
